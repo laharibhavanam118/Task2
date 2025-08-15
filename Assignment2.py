3ef158c62df2adc21db45ec89dd9fba00a9a583d
laharibhavanam118 <laharibhavanam118@gmail.com>
2025-08-15 22:03:58 +0530

    Add files via upload

--- Assignment2.py
+++ Assignment2.py
@@ -1,5 +1,5 @@
 #Prime Number
-'''n=int(input())
+n=int(input())
 count=0
 temp=n
 for i in range(1,n+1):
@@ -8,16 +8,16 @@ for i in range(1,n+1):
 if(count==2):
     print("prime")
 else:
-    print("not prime")'''
+    print("not prime")
 
 
 
 #Factorial of a number
-'''n=int(input())
+n=int(input())
 fact=1
 for i in range(1,n+1):
     fact=fact*i
-print(fact)'''
+print(fact)
 
 
 #Reverse the digits of a given number
@@ -33,4 +33,19 @@ print(s)
 #sum of digits of a given number
 n=int(input())
 s=0
-s=n[i]
+while n>0:
+    d=n%10
+    s=s+d
+    n=n//10
+print(s)
+
+#fibonacci series upto n terms
+n=int(input())
+n1=0
+n2=1
+print("Fibonacci series:",n1,n2,end=" ")
+for i in range(2,n):
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print(n3,end=" ")
