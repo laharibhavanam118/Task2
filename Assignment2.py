a9b92813bb88f3640613aaa6cfdca1e2a94b7a86
laharibhavanam118 <laharibhavanam118@gmail.com>
2025-08-15 21:51:19 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,36 @@
+#Prime Number
+'''n=int(input())
+count=0
+temp=n
+for i in range(1,n+1):
+    if(n%i==0):
+        count+=1
+if(count==2):
+    print("prime")
+else:
+    print("not prime")'''
+
+
+
+#Factorial of a number
+'''n=int(input())
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)'''
+
+
+#Reverse the digits of a given number
+n=int(input())
+s=0
+while(n!=0):
+    d=n%10
+    s=(s*10)+d
+    n=n//10
+print(s)
+
+
+#sum of digits of a given number
+n=int(input())
+s=0
+s=n[i]
